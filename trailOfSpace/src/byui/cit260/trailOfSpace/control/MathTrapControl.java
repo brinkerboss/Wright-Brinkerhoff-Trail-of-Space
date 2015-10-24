d379b9333a964e6b490d8bb5daae7fff2db639a8
animejedifreak <animejedifreak@pi-prime>
2015-10-23 22:31:33 -0600

    10/23/2015 Created the MathTrapControl classes.

---
+++ trailOfSpace/src/byui/cit260/trailOfSpace/control/MathTrapControl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.trailOfSpace.control;
+
+/**
+ *
+ * @author animejedifreak
+ */
+public class MathTrapControl {
+    
+    
+    public void mathTrap0(String equation, int solution) {
+    
+        //display greeting and equation
+    
+        if(solution == 28) {
+        //print success function
+        }
+        else {
+        //print failure function and recall equation display function
+        }
+    }
+
+
+
+ 
+
+
+    
+    
+}
