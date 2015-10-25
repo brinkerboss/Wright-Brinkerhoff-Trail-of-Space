b7e9875aefbb87eaee9303aeb3ea9488b3186b23
landonwright <landonwright@10.50.72.254>
2015-10-24 22:43:45 -0600

    10/24/2015 - UpgradeControl to repository

---
+++ trailOfSpace/src/byui/cit260/trailOfSpace/control/UpgradeControl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.trailOfSpace.control;
+
+/**
+ *
+ * @author landonwright
+ */
+public class UpgradeControl {
+  
+   public void upgradePlayerAttack(int attack){
+   
+   } 
+   
+   public void upgradePlayerDefense(int defense){
+   
+   }
+}
