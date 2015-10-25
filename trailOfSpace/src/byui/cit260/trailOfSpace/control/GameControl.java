2909303771716303cc374090e17d29261200ba09
landonwright <landonwright@10.50.72.254>
2015-10-24 22:38:15 -0600

    10/24/2015 - game class to repository

---
+++ trailOfSpace/src/byui/cit260/trailOfSpace/control/GameControl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.trailOfSpace.control;
+
+/**
+ *
+ * @author landonwright
+ */
+public class GameControl {
+    
+    
+    public void startNewGame(){
+    
+    }
+    
+    public void initialize(){
+    
+    }
+    
+    public void saveGame(){
+    
+    }
+    
+    public void loadGame(){
+    
+    }
+    
+    public void createNewPlayer(){
+    
+    }
+}
