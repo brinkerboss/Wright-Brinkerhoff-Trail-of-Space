492a2b362de53f2504b0420ddf4ee8c04d40c51c
animejedifreak <animejedifreak@172.16.216.1>
2015-11-10 09:58:31 -0700

    11/10/2015 Created the View superclass and the ViewInterface interface.

--- trailOfSpace/src/byui/cit260/trailOfSpace/control/BattleControl.java
+++ trailOfSpace/src/byui/cit260/trailOfSpace/control/BattleControl.java
@@ -17,7 +17,7 @@ public class BattleControl {
         int characterAttack;
         int enemyHealth;
         int enemyAttack;
-        int result = calcOutcome(characterHealth, characterAttack, enemyHealth, enemyAttack);
+        int result = this.calcOutcome(characterHealth, characterAttack, enemyHealth, enemyAttack);
         
         switch (result) {
             case -1:
