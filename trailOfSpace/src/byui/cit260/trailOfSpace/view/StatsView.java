1509271f77d2ac454d1b2c4b2cd4dadbb1e31f87
animejedifreak <animejedifreak@pi-prime>
2015-11-16 11:29:53 -0700

    11/15/2015 Created the StatsView class to display various stats of the user's character.

---
+++ trailOfSpace/src/byui/cit260/trailOfSpace/view/StatsView.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.trailOfSpace.view;
+
+/**
+ *
+ * @author animejedifreak
+ */
+public class StatsView extends View {
+    
+    public StatsView() {
+        super ("\n"
+            + "\n-----------------------------------------------------"
+            + "\n| Stats                                          |"
+            + "\n-----------------------------------------------------"
+            + "\n"
+            + "\n"
+            + "\nQ - Quit"
+            + "\n-----------------------------------------------------"
+            + "\n");
+    }
+    
+     @Override
+    public boolean doAction(Object obj) {
+        
+        String value = (String) obj;
+        value = value.toUpperCase();
+        char choice = value.charAt(0);
+        
+        switch (choice) {
+            case 'Q':
+                return false;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
